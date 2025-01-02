7e02e851ff95023e4abb1e3978c9ef193de130d3
Saimon <saimonribeiros@hotmail.com>
2025-01-01 22:31:14 -0300

    feat: init

--- internal/app/service/whatsApp_test.go
+++ internal/app/service/whatsApp_test.go
@@ -2,14 +2,20 @@ package service
 
 import (
 	"context"
-	"encoding/json"
 	"testing"
 
 	"github.com/inter-hubly/linker/internal/app/domain/dto/whatsapp"
+	"github.com/inter-hubly/pilot/server"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestWhatsApp(t *testing.T) {
+	server.NewMockEnvironment(
+		server.MockEnvironment{
+			WhatsAppToken: "EAAP8VwxXKXkBO3WJuZAX2ZBsW2mVzBAHqG7gb38x2fIgd3ydReFv6VVNRZBGnZBsmhOt8CF7R9msJgPSprnjS7dM7g6XQdfvo50dguTVp6NxZAzC6JX5SvDqqBv3CpHX39O1uEAkkCdBrbwE4NKbCBHKVNOC7dtc2OSFQWSnIx2HUbvCTFFbg3lXxFt3j4PQZA",
+		},
+	)
+
 	service := NewWhatsApp()
 
 	for _, v := range []struct {
@@ -21,33 +27,16 @@ func TestWhatsApp(t *testing.T) {
 		},
 	} {
 		t.Run(v.testName, func(t *testing.T) {
-			ctx := context.Background()
-			var entityWhats dto.dto
-			err := json.Unmarshal([]byte(jsonReceived), &entityWhats)
-			assert.Nil(t, err)
 
-			err = service.ChangeStatusMessage(ctx, &entityWhats)
+			ctx := context.Background()
+			err := service.SendMessage(ctx, &dto.SendTextDto{
+				Message: "Test message",
+				SenderAndReceiver: dto.SenderAndReceiverDto{
+					OwnerId: "559153210606318",
+					To:      "554888356622",
+				},
+			})
 			assert.Nil(t, err)
 		})
 	}
 }
-
-var jsonReceived = `
-{
-	"id": "510006955530686",
-	"sender": {
-			"phoneNumberId": "515719138282305",
-			"displayPhoneNumber": "15551817023"
-	},
-	"receive": {
-			"phoneNumberId": "510006955530686",
-			"displayPhoneNumber": "554891784586"
-	},
-	"metadata": {
-			"messageId": "wamid.HBgMNTU0ODkxNzg0NTg2FQIAERgSOTQyQjZBNEEwRjg3N0VGRURDAA==",
-			"recipientId": "554891784586",
-			"status": "read",
-			"body": "",
-			"timestamp": 1731695647
-	}
-}`
