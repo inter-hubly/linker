8b1249c427571e961a86e42e047c9e688b2731ec
Saimon <saimonribeiros@hotmail.com>
2025-04-26 18:59:47 -0300

    feat: templates

--- testutil/end2end_test.go
+++ testutil/end2end_test.go
@@ -2,30 +2,29 @@
 
 package testutil
 
-//
-// const withTestContainer = false
-//
-// func TestEnd2End(t *testing.T) {
-// 	var wg sync.WaitGroup
-// 	if withTestContainer {
-//
-// 	} else {
-// 		wg.Add(1)
-// 		go prepareTestEnvironment()
-// 	}
-//
-// 	time.Sleep(5 * time.Second)
-// 	message, err := SentMessage()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	rabbitmq.GetConnection().Publish("whatsapp.send", message)
-// 	wg.Wait()
-// 	// time.Sleep(5 * time.Second)
-// 	// wg.Done()
-// }
-//
-// func prepareTestEnvironment() {
-// 	server.MockStartEnv("../")
-// 	express.Start()
-// }
+const withTestContainer = false
+
+func TestEnd2End(t *testing.T) {
+	var wg sync.WaitGroup
+	if withTestContainer {
+
+	} else {
+		wg.Add(1)
+		go prepareTestEnvironment()
+	}
+
+	time.Sleep(5 * time.Second)
+	message, err := SentMessage()
+	if err != nil {
+		t.Fatal(err)
+	}
+	rabbitmq.GetConnection().Publish("whatsapp.send", message)
+	wg.Wait()
+	// time.Sleep(5 * time.Second)
+	// wg.Done()
+}
+
+func prepareTestEnvironment() {
+	server.MockStartEnv("../")
+	express.Start()
+}
--- testutil/whatsAppGateway_test.go
+++ testutil/whatsAppGateway_test.go
@@ -2,24 +2,24 @@
 
 package testutil
 
-// func TestWhatsAppGateway(t *testing.T) {
-// 	app := gateway.NewWhatsApp()
-// 	ctx := context.Background()
-// 	t.Run("send message to whatsapp", func(t *testing.T) {
-// 		messageTest := GetMessageTest()
-//
-// 		messageDto := dto.GatewayWhatsAppMessageDto{
-// 			MessagingProduct: "whatsapp",
-// 			RecipientType:    "individual",
-// 			To:               messageTest.Sender.PhoneNumberId,
-// 			Type:             "text",
-// 			Text: &dto.WhatsAppTextDto{
-// 				PreviewUrl: true,
-// 				Body:       messageTest.Metadata.Body,
-// 			},
-// 		}
-//
-// 		_, err := app.SendMessage(ctx, messageTest.Owner.PhoneNumberId, &messageDto)
-// 		assert.Nil(t, err)
-// 	})
-// }
+func TestWhatsAppGateway(t *testing.T) {
+	app := gateway.NewWhatsApp()
+	ctx := context.Background()
+	t.Run("send message to whatsapp", func(t *testing.T) {
+		messageTest := GetMessageTest()
+
+		messageDto := dto.GatewayWhatsAppMessageDto{
+			MessagingProduct: "whatsapp",
+			RecipientType:    "individual",
+			To:               messageTest.Sender.PhoneNumberId,
+			Type:             "text",
+			Text: &dto.WhatsAppTextDto{
+				PreviewUrl: true,
+				Body:       messageTest.Metadata.Body,
+			},
+		}
+
+		_, err := app.SendMessage(ctx, messageTest.Owner.PhoneNumberId, &messageDto)
+		assert.Nil(t, err)
+	})
+}
